8888afc9778809b3d21d54fa640820a5c82c2a0c
Orenimhe <osikwemhewisdom@gmail.com>
2022-09-19 19:50:49 -0700

    Main

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse something
+ * @s: params
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
